e8cd3dd1af606b023258dae8a34855657d842ff0
avaprin <sergey-gubkin@mail.ru>
2018-09-27 00:49:18 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
+    if((number < 1) || (number > Math.pow(10,8)))
+    {
+        return ("error! input number is incorrect");
+    }
+    let res = 0;
+    while(number > 0)
+    {
+        number= Math.floor(number/5);
+        res +=number;
+    }
+    return res;
   // your implementation
 }
